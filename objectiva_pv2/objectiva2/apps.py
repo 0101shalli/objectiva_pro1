5938110f7f26d4ec8652748d529923eb3c3a5e12
shalli <shallileroi@gmail.com>
2021-10-28 23:29:20 +0100

    Update README.md

---
+++ objectiva_pv2/objectiva2/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Objectiva2Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'objectiva2'
---
+++ objectiva_pv2/objectiva2/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request,"objectiva2/home.html")
+# Create your views here.
